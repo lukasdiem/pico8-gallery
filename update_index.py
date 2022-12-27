ec71d7d00bc1794a7ac298b2ec2d38c1dbc7b1e0
Lukas Diem <lukas.diem@htl-rankweil.at>
2022-12-27 20:15:11 +0100

    add static base_url

--- update_index.py
+++ update_index.py
@@ -6,7 +6,8 @@ import json
 def update_index():
     index_template = env.get_template('index.j2')
     carts = load_carts(path.join(config["Path"]["cart_dir"], 'carts.json'))
+    base_url = config["Web"]["base_url"]
     print("Found {} carts.".format(len(carts)))
-    index_result = index_template.render(carts=carts, links=get_links())
+    index_result = index_template.render(base_url=base_url, carts=carts, links=get_links())
     with open("dist/index.html", "w") as output:
         output.write(index_result)
--- update_plate.py
+++ update_plate.py
@@ -5,7 +5,8 @@ import json
 
 def update_plate():
     plate_template = env.get_template('pico8_plate.j2')
-    plate_result = plate_template.render()
-
+    base_url = config["Web"]["base_url"]
+    plate_result = plate_template.render(base_url=base_url)
+    
     with open("pico8_plate.html", "w") as output:
         output.write(plate_result)
